6eebb49db3a756160916ba66b0ef01daf901a858
David Velasco <davvelsan@gmail.com>
2020-03-10 16:19:16 +0100

    fix protractor getTitlteText (untested)

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
   }
 
   getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+    return element(by.css('span .ml-16')).getText() as Promise<string>;
   }
 }
