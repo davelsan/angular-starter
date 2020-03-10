e79202f5155129eb15884898eedb4726bbd51f59
David Velasco <davvelsan@gmail.com>
2020-03-10 13:30:55 +0100

    fixed failing unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { TestBed, async  } from '@angular/core/testing';
+import { AppComponent    } from './app.component';
+import { AppSvgComponent } from './app-svg/app-svg.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        AppSvgComponent
       ],
     }).compileComponents();
   }));
@@ -16,16 +18,21 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should have as title "homepage-ng"', () => {
+  it('should have as title "angular-tailwind-eslint"', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('homepage-ng');
+    expect(app.title).toEqual('angular-tailwind-eslint');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('homepage-ng app is running!');
+    const element = fixture.debugElement.nativeElement;
+    expect(
+      element
+        .querySelector('.ml-16')
+        .textContent
+    )
+    .toContain('angular-tailwind-eslint app is running!');
   });
 });
