01a128f47c6c816cd33b8b9cbe57f89ae457a51b
David Velasco <davvelsan@gmail.com>
2020-03-10 16:13:29 +0100

    protractor config in ts (untested)

--- e2e/protractor.conf.ts
+++ e2e/protractor.conf.ts
@@ -1,13 +1,11 @@
-// @ts-check
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-const { SpecReporter } = require('jasmine-spec-reporter');
+import * as Path        from 'path';
+import { register }     from 'ts-node';
+import { SpecReporter } from 'jasmine-spec-reporter';
 
-/**
- * @type { import("protractor").Config }
- */
-exports.config = {
+export const config = {
   allScriptsTimeout: 11000,
   specs: [
     './src/**/*.e2e-spec.ts'
@@ -24,8 +22,8 @@ exports.config = {
     print: function() {}
   },
   onPrepare() {
-    require('ts-node').register({
-      project: require('path').join(__dirname, './tsconfig.json')
+    register({
+      project: Path.join(__dirname, './tsconfig.json')
     });
     jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
   }
