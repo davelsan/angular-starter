e57515b135613ee9de83c6f4a42f22b18cd02d2b
David Velasco <davvelsan@gmail.com>
2020-03-11 20:22:07 +0100

    feat: add purgecss

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,14 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+  // Specify the paths to all of the template files in your project
+  content: [
+    './src/**/*.html',
+  ],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
 module.exports = {
   module: {
     rules: [
@@ -9,6 +20,7 @@ module.exports = {
           plugins: () => [
             require("postcss-import"),
             require("tailwindcss"),
+            purgecss,
             require("autoprefixer")
           ]
         }
