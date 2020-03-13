3ea712ac1801dda656480006563465199081eb34
David Velasco <davvelsan@gmail.com>
2020-03-13 10:58:23 +0100

    fix: app tests had not been ported to app-home component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,11 @@
 import { TestBed, async  } from '@angular/core/testing';
 import { AppComponent    } from './app.component';
-import { AppSvgComponent } from './shared/app-svg/app-svg.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent,
-        AppSvgComponent
       ],
     }).compileComponents();
   }));
@@ -16,23 +14,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it('should have as title "angular-tailwind-eslint"', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-tailwind-eslint');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const element: HTMLElement = fixture.debugElement.nativeElement;
-    expect(
-      element
-        .querySelector('.ml-16')
-        .textContent
-    )
-    .toContain('angular-tailwind-eslint app is running!');
   });
 });
--- src/app/modules/home/app-home/app-home.component.spec.ts
+++ src/app/modules/home/app-home/app-home.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+/* COMPONENT */
 import { AppHomeComponent } from './app-home.component';
+
+/* DEPENDENCIES */
+import { AppSvgComponent } from '@shared/app-svg/app-svg.component';
+
 
 describe('AppHomeComponent', () => {
   let component: AppHomeComponent;
@@ -8,7 +13,10 @@ describe('AppHomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AppHomeComponent ]
+      declarations: [
+        AppHomeComponent,
+        AppSvgComponent,
+      ]
     })
     .compileComponents();
   }));
@@ -21,5 +29,21 @@ describe('AppHomeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have as title "angular-tailwind-eslint"', () => {
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('angular-tailwind-eslint');
+  });
+
+  it('should render title', () => {
+    fixture.detectChanges();
+    const element: HTMLElement = fixture.debugElement.nativeElement;
+    expect(
+      element
+        .querySelector('.ml-16')
+        .textContent
+    )
+    .toContain('angular-tailwind-eslint app is running!');
   });
 });
